2ecaf89e73b51d9a4bca2fa9f2df1e42c49ad70a
thanhhau <trannguyenthanhhaupro09@gmail.com>
2025-02-16 21:14:34 +0700

    reponse trang home cho table

--- index.js
+++ index.js
@@ -10,14 +10,3 @@ headerMenuMobile1NavSearch.addEventListener("click", () => {
   headerMenuMobile2.style.display = "flex";
 });
 
-fni.forEach(fniTest => {
-    fniTest.addEventListener("click", ()=> {
-        alert("Đây là bản demo giao diện, không thể tương tác. Vui lòng quay lại sau !")
-    })
-})
-
-fna.forEach(fnaTest => {
-    fnaTest.addEventListener("click", ()=> {
-        alert("Đây là bản demo giao diện, không thể tương tác. Vui lòng quay lại sau !")
-    })
-})
